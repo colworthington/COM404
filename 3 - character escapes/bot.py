ea8cea7896bb2364675e0e72726dfbb0ad1d7619
colworthington <4wortc73@solent.ac.uk>
2019-09-29 19:21:43 +0000

    String Operators

--- 3 - character escapes/bot.py
+++
@@ -1,6 +0,0 @@
-# Display escape characters
-print("\n Displays a new line")
-print("\t Displays a tab space")
-print("\\ Dsplays back slash")
-print("\" Displays a double quote")
-print("\' Displays a single quote")
