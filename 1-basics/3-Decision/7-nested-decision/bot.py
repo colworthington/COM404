2584b194386125281180481c4ef586671ac5e65e
colworthington <4wortc73@solent.ac.uk>
2019-10-01 11:02:35 +0000

    saving

---
+++ 1-basics/3-Decision/7-nested-decision/bot.py
@@ -0,0 +1,13 @@
+# Allows Beep to classify his books
+cover = (input("What type of cover does the book have? (hard or soft)"))
+    
+# soft or hard Cover options
+if (cover == "soft"):
+
+    # ask user what to play with 
+    if (play_with == "toy"):
+        print("I will play with my toys!")
+    else:
+        print("I will play with my friend!")
+else:
+    print("I will study")
