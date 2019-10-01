dcc2d96524e89e0ee8d3b0f3a6b850144f562f95
colworthington <4wortc73@solent.ac.uk>
2019-10-01 08:45:55 +0000

    Added code to demonstrate the use of an if statement.

---
+++ 1-basics/3-Decision/1-if/bot.py
@@ -0,0 +1,9 @@
+# Type of book
+print("What type of book is this?")
+book = input()
+if (book == "adventure"):
+    print("I like adventure books!")
+    print()
+
+print()
+print("Finished reading book!")
