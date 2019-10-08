45c04cecdf89c3fb9524a71bd30c8b0dcdf784a1
colworthington <4wortc73@solent.ac.uk>
2019-10-08 08:45:28 +0000

    Added code to demonstrate the use of a simple while loop.

---
+++ 1-basics/4-repetition/1-while-loop/1-simple/bot.py
@@ -0,0 +1,9 @@
+# While Loop for Beep to remove the cables holding the robot.
+cables = int(input("How many cables should I remove? "))
+removed = 0
+print("\n")
+while removed < cables:
+    print("Removed cable.")
+    removed = removed +1
+print()
+print ("Completed")
