e3b57dbf81ac6c3ea3f28ee518802141b0dcbbb2
colworthington <4wortc73@solent.ac.uk>
2019-10-08 09:54:42 +0000

    Added code to demonstrate the use of a while loop with len function.

---
+++ 1-basics/4-repetition/1-while-loop/4-len/bot.py
@@ -0,0 +1,9 @@
+# while loop with len function - help robot to communicate with Beep
+phrase = (input("Please enter a phrase: "))
+char = len(phrase)
+count = 0
+print("\n")
+while  count < char:
+        print(" Bop",end="")
+        count = count +1
+print("\n\n End.")
