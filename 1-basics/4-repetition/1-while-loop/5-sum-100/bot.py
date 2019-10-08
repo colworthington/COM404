2d239095c25c4d32f33ca890c8ed9279cbae16e1
colworthington <4wortc73@solent.ac.uk>
2019-10-08 10:27:54 +0000

    performing calculations that involve repetitive steps

---
+++ 1-basics/4-repetition/1-while-loop/5-sum-100/bot.py
@@ -0,0 +1,11 @@
+# Help Bop calculate the sum of the first 100 numbers
+total = 0
+count = 1
+print("Calculating the sum of the first 100 numbers... ")
+print()
+while count <= 100:
+    total = total + count
+    count = count +1
+
+print("...Done! The answer is " + str(total) + "")
+print()
