c6926de34817dd6b24bcfb9b4f25b1775d16e48c
colworthington <4wortc73@solent.ac.uk>
2019-10-11 21:57:35 +0000

    Added code to demonstrate the use of one loop nested is another

--- 1-basics/4-repetition/3-nested-loop/1-nested/bot.py
+++ 1-basics/4-repetition/3-nested-loop/1-nested/bot.py
@@ -1,13 +1,13 @@
 # code to demonstrate the use of one loop nested in another
-rows = (input("How many rows should I have? "))
+rows = int(input("How many rows should I have? "))
 print()
-columns = (input("How many columns should I have? "))
+columns = int(input("How many columns should I have? "))
 print()
 print("Here I go...")
+print()
 for count in range(0, rows, 1):
     for number in range(0, columns, 1):
-        print(number, "", end="")
+        print(number*":-) ", end="")
     print()
 
-print()
-print("End.")
+print("\nDone!")
