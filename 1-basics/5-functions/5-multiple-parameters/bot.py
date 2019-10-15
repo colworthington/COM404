7c61c854acd96c12a48ce1a98cc9867f00cd5a15
colworthington <4wortc73@solent.ac.uk>
2019-10-15 10:44:26 +0000

    code to demonstrate the use of a user-defined function with multiple parameters

---
+++ 1-basics/5-functions/5-multiple-parameters/bot.py
@@ -0,0 +1,13 @@
+# code to demonstrate the use of a user-defined function with multiple parameters
+print()
+# defining the function
+def climb_ladder(remaining, crossed):
+    if int(crossed) > int(remaining):
+        print("Still some way to go!")
+        return climb_ladder
+    else:
+        print("We made it!")
+
+# calling the function
+climb_ladder(2, 5)
+climb_ladder(5, 5)
