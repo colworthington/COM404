5e5c057a2634205289679f08f9b5d4e444ffb847
colworthington <4wortc73@solent.ac.uk>
2019-10-20 21:43:41 +0000

    code to demonstrate the use of return values

--- 1-basics/5-functions/7-return-values/bot.py
+++ 1-basics/5-functions/7-return-values/bot.py
@@ -1,24 +1,26 @@
 # code to demonstrate the use of return values
 print()
 # defining the function
-def sum_weights(beepWeight, bopWeight):
-    sum_weights = int(beepweight) + int(bopweight) 
-    return sum_weights    
-def calc_avg_weight(beepWeight, bopWeight):
-    calc_avg_weight = sum_weights()
-    return calc_avg_weight
+def sum_weights(beepweight, bopweight):
+    return beepweight + bopweight  
+def calc_avg_weight(beepweight, bopweight):
+    total = sum_weights(beepweight, bopweight)
+    return total / 2
 def run():
     beepweight = int(input("What is the weight of Beep? "))
+    print()
     bopweight = int(input("What is the weight of Bop? "))
+    print()
     response = input("What would you like to calcluate (sum or average)? ")
     if (response == "sum"):
-        sum_weights()
-        # print("total weight = " sum_weights()"")
+        print()
+        print("The sum of Beep and Bop's weight is:", sum_weights(beepweight, bopweight))
     elif (response == "average"):
         print()
-        calc_avg_weight()
+        print("The average of Beep and Bop's weight is:", calc_avg_weight(beepweight, bopweight))
         # print("average weight = " calc_avg_weight()"")
     else:
         print("I don't know that one!")
 # Run the program
 run()
+print()
