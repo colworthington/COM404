02a44de87f160c757221a66926ba1c333b520d7a
colworthington <4wortc73@solent.ac.uk>
2019-10-22 08:14:56 +0000

    Question1

---
+++ TCA - Test/Input and Output/q1.py
@@ -0,0 +1,6 @@
+# input and output program]#
+print()
+# reading users input
+response = input("What happens when the last petal falls? ")
+print("My dear Bella when the last petal falls " + str(response))
+print()
