fea56df99d2a2426c25ab5ea5a2700a6f5c90dbd
colworthington <4wortc73@solent.ac.uk>
2019-10-22 08:25:05 +0000

    Question2

---
+++ TCA - Test/2. Decision/q2.py
@@ -0,0 +1,10 @@
+# Decision
+print()
+# reading users input
+question = input("Have you fear in your heart? ")
+if question == "yes":
+    print("Fear is the path to the dark side. You cannot be a Jedi apprentice.")
+else:
+    print("The force is strong in you. You may be a Jedi apprentice.")
+
+print()
