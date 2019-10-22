582c93e2d03819d79ea5a682fdf762e6f4812cea
colworthington <4wortc73@solent.ac.uk>
2019-10-22 11:26:56 +0000

    TCA

--- TCA - Test/7. Modules/functions.py
+++ TCA - Test/7. Modules/functions.py
@@ -1,10 +1,10 @@
 # defining the functions
-def under(word):
+def under():
     print("", word)
     for position in range(0, len(word), 1):
-        print("" + str(position) + ,end="")
+        print(" " + str(position) + "" ,end="")
 
-def over(word):
+def over():
     print("", word)
     for position in range(0, len(word), 1):
         print("" + str(position) + "*" ,end="")
