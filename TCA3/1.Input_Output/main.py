6a6e8603b1c2221820a26cc281c1b5ae3fa90365
colworthington <4wortc73@solent.ac.uk>
2019-10-23 20:18:00 +0000

    InputandOutput

---
+++ TCA3/1.Input_Output/main.py
@@ -0,0 +1,7 @@
+# code to demonstrate input and output
+# set variable and ask question - input
+print()
+name = input("What is your name my honourable guest? ")
+# print desired output
+print("\nMi casa es su casa " + name)
+print()
