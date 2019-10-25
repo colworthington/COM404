ba4bd48607c972118c690bdac50bd02e652157fb
colworthington <4wortc73@solent.ac.uk>
2019-10-25 21:28:22 +0000

    factorial

--- 1-basics/4-repetition/1-while-loop/7-factorial/bot.py
+++ 1-basics/4-repetition/1-while-loop/7-factorial/bot.py
@@ -1,11 +1,10 @@
 # a while loop in calculating the factorial of a number
 number = int(input("Please enter a number: "))
 # sets count at number -1
-count = number-1
-
-answer = number
+count = number -1
+total = number
 while count > 0 :
-    total = answer * count
-    count = count -1
+    total = total * count
+    count = count -1    
     
 print("\n The factorial is " +str(total) + "")
