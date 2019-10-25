e6d0ae7541e2a00eaf0ffe844b515c8cc6880735
colworthington <4wortc73@solent.ac.uk>
2019-10-25 20:35:40 +0000

    count

--- 1-basics/4-repetition/1-while-loop/2-count/bot.py
+++ 1-basics/4-repetition/1-while-loop/2-count/bot.py
@@ -1,11 +1,12 @@
 # While Loop with counter for Beep to remove the cables holding the robot.
 avoid = int(input("How many live cables must I avoid? "))
 live = 0
-count = 0
+#count = 0
 print("\n")
 while live < avoid:
-    count = count +1
-    print("Avoiding...Done! " + str(count) + " live cables avoided.")
-    live = live +1  
+    #count = count +1
+    live = live +1 
+    print("Avoiding...Done! " + str(live) + " live cables avoided.")
+     
 print()
 print ("Completed")
