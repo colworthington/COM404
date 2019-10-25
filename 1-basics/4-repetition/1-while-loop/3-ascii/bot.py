55aa130b992b9a8b0f058703d0ee7e773a9990ca
colworthington <4wortc73@solent.ac.uk>
2019-10-25 20:44:30 +0000

    While-Loop

--- 1-basics/4-repetition/1-while-loop/3-ascii/bot.py
+++ 1-basics/4-repetition/1-while-loop/3-ascii/bot.py
@@ -1,12 +1,13 @@
 # while loop with ascii art - allows Beep to avoid live cables
 bars = int(input("How many bars should be charged? "))
 charging = 0
-count = 0
-print("\n")
+#count = 0
+print()
 while  charging < bars:
-    count = count +1
-    print("Charging: " + str(count*" █"))
-    charging = charging +1  
+    #count = count +1
+    charging = charging +1
+    # print("Charging: " + str(count*" █"))
+    print("Charging: " + str(charging*" █"))
 print()
 print ("The battery is fully charged.")
 print()
