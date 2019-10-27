903bcd4970ce799599639a574fdde5d1e93af11e
colworthington <4wortc73@solent.ac.uk>
2019-10-27 14:54:34 +0000

    Loops

---
+++ TCA-Test2/3.Loop/main.py
@@ -0,0 +1,9 @@
+# Code to demonstrate Loops
+print()
+number = int(input("How many days remain until the next full moon? "))
+print("We must count the days... \n")
+while number > 0:
+    print("The full moon will be upon us in " + str(number) + " days.")
+    number = number -1
+print("\nIt's a full moon. The beast has been unleashed!\nMay it have mercy on our souls.")
+print()
