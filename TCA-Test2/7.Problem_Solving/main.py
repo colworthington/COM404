343b79e7d340b2ac1979ed9b697afc92a4da0f66
colworthington <4wortc73@solent.ac.uk>
2019-10-27 18:36:24 +0000

    ProblemSolving

--- TCA-Test2/7.Problem_Solving/main.py
+++ TCA-Test2/7.Problem_Solving/main.py
@@ -10,19 +10,51 @@ def over(message):
     print("\n" + str(message))
     print(count * "-", end="")
     print("\nHalloween")
+
+def both(message):  
+    count = len(message)
+    print("\n" + str(message))
+    print(count * "-", end="")
+    print("\nHalloween")
+    print(count * "-", end="")
+    print("\n" + str(message))
     
 
-message = input("Enter a message: ")
+def grid(value):
+    if value > 5:
+        value = 5
+    count2 = value
+    print()
+    while count2 != 0:
+        for count in range(value):
+            print("Halloween", end="")
+            print("|", end="")
+        count2 = count2 -1
+        print()
+    print()
+
 print("\nEnter one of the following options:")
 print(" (1) Under - display the message with a line under it.")
 print(" (2) Over - display the message with a line over it")
 print(" (3) Both - display the message in an underline and overline")
-print(" (4) Grid - display the message in a grid that is n x n in size.")
+print(" (4) Grid - display Halloween in a grid that is n x n in size.")
 option = input("Enter 1 to 4: ")
 if option == "1":
+    message = input("Enter a message: ")
     under(message)
     print()
 elif option == "2":
+    message = input("Enter a message: ")
     over(message)
     print()
+elif option == "3":
+    message = input("Enter a message: ")
+    both(message)
+    print()
+elif option == "4":
+    value = int(input("Please enter value for grid: "))
+    grid(value)
+else:
+    print("\nI do not know that option.")
+
 
