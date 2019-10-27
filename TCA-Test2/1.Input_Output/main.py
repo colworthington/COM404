5c2e77fb8f3a3b1c2e90f74521fcba19595e2199
colworthington <4wortc73@solent.ac.uk>
2019-10-27 14:39:54 +0000

    InputOutput

---
+++ TCA-Test2/1.Input_Output/main.py
@@ -0,0 +1,5 @@
+# Code to demonstrate input and output commands
+# reading in user response
+response = input("\nTrick or Treat? ")
+# output message with response
+print("It will be a ghostly " + response + "\n")
