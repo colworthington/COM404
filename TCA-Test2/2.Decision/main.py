33d71b21805b06888c17588f659cf78e9779d6d7
colworthington <4wortc73@solent.ac.uk>
2019-10-27 14:46:46 +0000

    decision

---
+++ TCA-Test2/2.Decision/main.py
@@ -0,0 +1,10 @@
+# Code to demonstrate if..elif..else statement
+response = input("\nWhat type of night be it tonight? ")
+# checking the user response
+if response == "lightning":
+    print("It is time to be ALIVE!")
+elif response == "cloudy":
+    print("The sky is stirring but we must wait.")
+else:
+    print("Soon my precious. Soon.")
+print()
