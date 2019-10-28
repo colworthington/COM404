928a7d974b01c77431d27452de3f194abf875b18
colworthington <4wortc73@solent.ac.uk>
2019-10-28 21:35:10 +0000

    FunctionCalls

--- 1-basics/5-functions/8-function-calls/bot.py
+++ 1-basics/5-functions/8-function-calls/bot.py
@@ -1,19 +1,66 @@
 # code to demonstrate the use of multiple function calls
 print()
-word = "Hello"
-def ascii_box(number):
-    for count in range(number, 0, -1):
-        print("##########")
-        print("# " + str(word) + " #")
-        print("##########")
-def lower_case(number):
-    print("")
-def upper_case(number):
-    print("")
-def mirrored(number):
-    print("")
-def run():
-    number = int(input("How many times to display word? "))
+def ascii_box(word):
     print()
-    ascii_box(number)
+    for position in range(0, len(word)+4, 1):
+        print("#", end="")
+    print("\n# " + str(word) + " #")
+    for position in range(0, len(word)+4, 1):
+        print("#", end="")
+    print()
+
+def lower_case(word):
+    print()
+    new_word = word.lower()
+    print ("lower case: "+ new_word)
+
+def upper_case(word):
+    print()
+    new_word = word.upper()
+    print ("UPPER case: "+ new_word)
+    
+def mirrored(word):
+    print()
+    reversed = ""
+    for letter in word:
+        reversed = letter + reversed
+    print("Mirrored: " + word + " |", reversed)
+    
+def repeat(number, word):
+    while number > 0:
+        print()
+        new_word = word.lower()
+        print ("lower case: "+ new_word)
+        number = number -1
+        new_word = word.upper()
+        print ("UPPER case: "+ new_word)
+        
+
+def run():
+    word = input("Enter a word: ")
+    print("\nEnter one of the following options:")
+    print(" (1) Display the word in a box.")
+    print(" (2) Display the word in lower case.")
+    print(" (3) Display a word in UPPER case")
+    print(" (4) Display word mirrored.")
+    print(" (5) Repeat the word.")
+    option = input("Enter 1 to 5: ")
+    if option == "1":
+        ascii_box(word)
+        print()
+    elif option == "2":
+        lower_case(word)
+        print()
+    elif option == "3":
+        upper_case(word)
+        print()
+    elif option == "4":
+        mirrored(word)
+        print()
+    elif option == "5":
+        number = int(input("How many times to display word? "))
+        repeat(number, word)
+    else:
+        print("\nI do not know that option.")
+    
 run()
