bd8fe71dc05946b4d56ce75e3a2011edd90bdbfd
colworthington <4wortc73@solent.ac.uk>
2019-11-05 12:27:11 +0000

    code to demonstrate inheritance

---
+++ 2-guis/1-classes-and-objects/SuperBot.py
@@ -0,0 +1,6 @@
+from Bot import *
+class SuperBot():
+    def __init__(self, name, age, energy, shield_level, super_power_level =0):
+        super().__init__(name, age, energy, shield_level)
+        self.super_power_level = 5
+        print()
---
+++ 2-guis/1-classes-and-objects/main.py
@@ -0,0 +1,9 @@
+# Code to demonstrate inheritance
+from Bot import Bot
+#from SuperBot import *
+#display_name, display_age, display_energy
+
+
+
+bop = Bot()
+#bop.display_name
