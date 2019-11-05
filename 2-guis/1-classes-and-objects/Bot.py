7f5c6fd1ae174de27455d4cd4d9facc79812f4dd
colworthington <4wortc73@solent.ac.uk>
2019-11-05 11:31:52 +0000

    code to demonstrate the implementation of a class and object

---
+++ 2-guis/1-classes-and-objects/Bot.py
@@ -0,0 +1,45 @@
+# Code to demonstrate the implementation of a class and object
+class Bot:
+    def __init__(self):
+        self.name = "Bop"
+        self.age = " 30 "
+        self.energy = "5"
+        self.shield_level = "8"
+        self.summary = ""
+
+    def display_name(self):
+        print()
+        print(" #########")
+        print(" #  {}  #".format(self.name))
+        print(" #########") 
+        
+    def display_age(self):
+        print()
+        print("    {}  ".format(self.age))
+        print("     ##")
+        print("    ####")
+        print(" ##########")
+        print(" #  ####  #")
+        print(" ##########")
+        print()
+
+    def display_energy(self):
+        total = int(self.energy)
+        print("Energy: " + total*"♦")
+        print()
+
+    def display_shield(self):
+        total2 = int(self.shield_level)
+        print("Shield: " + total2*"█")
+        print()
+
+    def __str__(self):
+        return("{} is{}years old, energy: {} , shield: {} \n".format(self.name, self.age, self.energy, self.shield_level))
+        
+
+bop = Bot()
+bop.display_name()
+bop.display_age()
+bop.display_energy()
+bop.display_shield()
+print(bop.__str__())
