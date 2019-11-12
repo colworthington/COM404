cba2d522ec6595b8624d54a30c0db976e123657d
Colin Worthington <4wortc73@solent.ac.uk>
2019-11-12 12:32:36 +0000

    code to display a GUI using the pack method

--- 2-guis/2-window-layouts/2-pack/gui2.py
+++ 2-guis/2-window-layouts/2-pack/gui2.py
@@ -13,9 +13,10 @@ class Gui(Tk):
         self.configure(bg="#eee", height=225, width=400)
         #self.title("Gui")
         #self.geometry("500x500")
-        self.add_background_canvas()
+        #self.add_background_canvas()
         self.add_heading_label()
         self.add_message_label()
+        self.add_email_frame()
         self.add_emailtext_label()
         self.add_email_entry()
         self.add_subscribe_button()
@@ -26,7 +27,7 @@ class Gui(Tk):
         # add window components by
         # ...creating an object of the component stored in an attribute
         self.heading_label = Label()
-        self.heading_label.place(x=46, y=7)
+        self.heading_label.pack()
         # style...
         # ...setting the attributes of the component using the attribute
         # self.heading_label.configure(font="Calibri 24", text="This is a Heading.")
@@ -37,7 +38,7 @@ class Gui(Tk):
 # Message function
     def add_message_label(self):
         self.message_label = Label()
-        self.message_label.place(x=24, y=60)
+        self.message_label.pack()
       
         # style...
         self.message_label.configure(font="Arial 10", text="Please enter your Email below to receive our newsletter...")
@@ -45,26 +46,32 @@ class Gui(Tk):
   # handle events
   # (callback functions to handle events will go here)
 
+# Email frame function
+    def add_email_frame(self):
+        self.email_frame = Frame()
+        self.email_frame.pack()
+
 # Email Text function
     def add_emailtext_label(self):
-        self.emailtext_label = Label()
-        self.emailtext_label.place(x=34, y=108)
-        self.emailtext_label.configure(font="Arial 10", text="Email: ")
+        self.emailtext_label = Label(self.email_frame)
+        self.emailtext_label.pack(side=LEFT)
+        self.emailtext_label.configure(font="Arial 10", text="Email:")
 
 # Email Entry function
     def add_email_entry(self):
-        self.email_entry = Entry(width=45)
-        self.email_entry.place(x=86, y=108)
+        self.email_entry = Entry(self.email_frame)
+        self.email_entry.pack(side=RIGHT)
 
-    def add_background_canvas(self):
-        self.background_canvas = Canvas(width=400)
-        self.background_canvas.place(x=0, y=0)
-        self.background_canvas.configure(bg="#e0eeee")
+    #def add_background_canvas(self):
+        #self.background_canvas = Canvas(width=400)
+        #self.background_canvas.pack
+        #self.background_canvas.configure(bg="#e0eeee")
 
 # Button function
     def add_subscribe_button(self):
         self.subscribe_button = Button(width=40)
-        self.subscribe_button.place(x=15, y=170)
+        self.subscribe_button.pack()
+        #self.subscribe_button.place(x=15, y=170)
         self.subscribe_button.configure(bg="#ffe4c4", font="Arial 11", text="Subscribe")
 
 
