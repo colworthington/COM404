e235ad849494fa7baae57590fa2a52b4c17d2d8f
colwor <55738001+colw@users.noreply.github.com>
2019-12-09 20:56:11 +0000

    multiple comp

--- 2-guis/5-animation/2-multiple-components/gui2.py
+++ 2-guis/5-animation/2-multiple-components/gui2.py
@@ -5,18 +5,13 @@ import time
 class AnimatedGui(Tk):
     def __init__(self):
         super().__init__()
-              
+        
         # load resources
         self.red_ball_image = PhotoImage(file="C:/Users/kkcolin.ENTERPRISE/Documents/GitHub/COM404/2-guis/5-animation/2-multiple-components/red_ball.gif")
         self.blue_ball_image = PhotoImage(file="C:/Users/kkcolin.ENTERPRISE/Documents/GitHub/COM404/2-guis/5-animation/2-multiple-components/blue_ball.gif")
-
+        
         # set window attributes
-        #self.configure(height=500, width=500)
-
-        # set window attributes
-        self.title("Animation")
-        self.configure(bg="#ccc", height=500, width=500)
-        #padx=10, pady=10)
+        self.configure(height=500, width=500)
 
         # set animation attributes
         self.red_ball_x_pos = 80
@@ -28,75 +23,62 @@ class AnimatedGui(Tk):
         self.blue_ball_y_pos = 300
         self.blue_ball_x_change = 5
         self.blue_ball_y_change = 5
-        
+
         # add components
         self.add_red_ball_image_label() 
         self.add_blue_ball_image_label() 
-        
+
         # start the timer
         self.tick()
-
-# the timer tick function    
+        
+    # the timer tick function    
     def tick(self):
-         #if self.red_ball_x_pos  > 450:
-            #self.red_ball_x_change = -5
 
-        #if self.red_ball_y_pos > 450:
-            #self.red_ball_y_change = -5
-        
-        #if self.red_ball_x_pos  < 0:
-           # self.red_ball_x_change = 5
-
-        #if self.red_ball_y_pos < 0:
-            #self.red_ball_y_change = 5
-
-        #if self.blue_ball_x_pos  > 450:
-           # self.blue_ball_x_change = -5
-
-        #if self.blue_ball_y_pos > 450:
-            #self.blue_ball_y_change = -5
-        
-        #if self.blue_ball_x_pos  < 0:
-            #self.blue_ball_x_change = 5
-
-        if self.red_ball_x_pos > 450 or self.blue_ball_x_pos > 450:
+        if self.red_ball_x_pos  > 450:
             self.red_ball_x_change = -5
-            self.blue_ball_x_change = -5
-            
-        if self.red_ball_y_pos > 450 or self.blue_ball_y_pos > 450:
-            self.red_ball_y_change = -5
-            self.blue_ball_y_change = -5
 
-        if self.red_ball_x_pos < 0 or self.blue_ball_x_pos < 0:
+        if self.red_ball_y_pos > 450:
+            self.red_ball_y_change = -5
+        
+        if self.red_ball_x_pos  < 0:
             self.red_ball_x_change = 5
+
+        if self.red_ball_y_pos < 0:
+            self.red_ball_y_change = 5
+
+        if self.blue_ball_x_pos  > 450:
+            self.blue_ball_x_change = -5
+
+        if self.blue_ball_y_pos > 450:
+            self.blue_ball_y_change = -5
+        
+        if self.blue_ball_x_pos  < 0:
             self.blue_ball_x_change = 5
 
-        if self.red_ball_y_pos < 0 or self.blue_ball_x_pos < 0:
-            self.red_ball_y_change = 5
+        if self.blue_ball_y_pos < 0:
             self.blue_ball_y_change = 5
 
         self.red_ball_x_pos = self.red_ball_x_pos + self.red_ball_x_change
         self.red_ball_y_pos = self.red_ball_y_pos + self.red_ball_y_change
         self.red_ball_image_label.place(x=self.red_ball_x_pos, y=self.red_ball_y_pos)
-
         self.blue_ball_x_pos = self.blue_ball_x_pos + self.blue_ball_x_change
         self.blue_ball_y_pos = self.blue_ball_y_pos + self.blue_ball_y_change
         self.blue_ball_image_label.place(x=self.blue_ball_x_pos, y=self.blue_ball_y_pos)
-       
         self.after(75, self.tick)
 
-# the ball image
+    # the red ball image
     def add_red_ball_image_label(self):
         self.red_ball_image_label = Label()
         self.red_ball_image_label.place(x=self.red_ball_x_pos, y=self.red_ball_y_pos)
         self.red_ball_image_label.configure(image=self.red_ball_image)
 
+    # the blue ball image
     def add_blue_ball_image_label(self):
         self.blue_ball_image_label = Label()
         self.blue_ball_image_label.place(x=self.blue_ball_x_pos, y=self.blue_ball_y_pos)
         self.blue_ball_image_label.configure(image=self.blue_ball_image)
 
-        
-if (__name__ == "__main__"):
-    gui = AnimatedGui()
-    gui.mainloop()
+# the object
+if __name__ == "__main__":
+    gui2 = AnimatedGui()    
+    gui2.mainloop() 
