c7a2eefe6ba696d3af64d169cf42875e0e33f3f3
colwor <55738001+colw@users.noreply.github.com>
2019-12-10 15:04:41 +0000

    Santa

--- AE2/part_a.py
+++ AE2/part_a.py
@@ -8,17 +8,17 @@ class Gui(Tk):
         super().__init__()
 
         # load resources
-        self.santa_image = PhotoImage(file="//pclures06/home/4wortc73/Documents/Problem Solving/COM404/AE2/santa.gif")
-         
+        #self.santa_image = PhotoImage(file="//pclures06/home/4wortc73/Documents/Problem Solving/COM404/AE2/santa.gif")
+        self.santa_image = PhotoImage(file="C:/Users/kkcolin.ENTERPRISE/Documents/GitHub/COM404/AE2/santa.gif")
+
         # set window properties
         self.title("Letter to Santa")
-        self.configure(bg="#f66", height=220, width=400, padx=15, pady=15)
+        self.configure(bg="#f66", height=400, width=400, padx=15, pady=15)
 
         # add components
         self.__add_global_frame()
         self.__add_heading_label()
-        self.__add_heading_label()
-        self.__add_name_frame()
+        #self.__add_name_frame()
         self.__add_name_label()
         self.__add_name_entry()
         #self.__add_santa_frame()
@@ -37,21 +37,24 @@ class Gui(Tk):
         self.heading_label.configure(bg="#f33", fg="#fff", font="Arial 18", text="Write to Santa!", padx=5, pady=5)
     
     # create name Frame
-    def __add_name_frame(self):
-        self.name_frame = Frame(self.global_frame)
-        self.name_frame.grid(row=1, column=0)
-        self.name_frame.configure(bg="#f33")
+    #def __add_name_frame(self):
+       # self.name_frame = Frame(self.global_frame)
+        #self.name_frame.grid(row=1, column=0)
+        #self.name_frame.configure(bg="#f33")
 
     def __add_name_label(self):
-        self.name_label = Label(self.name_frame)
-        self.name_label.pack(side=LEFT)
+        self.name_label = Label(self.global_frame)
+        self.name_label.grid(row=1, column=0, sticky=W)
+        #self.name_label.pack(side=LEFT)
         self.name_label.configure(bg="#f33", fg="#fff", font="Arial 12", text="Your name:", padx=5, pady=5)
 
     # Name Entry function
     def __add_name_entry(self):
-        self.name_entry = Entry(self.name_frame)
-        self.name_entry.pack(side=RIGHT)
-        self.name_entry.configure(fg="black", width=40)
+        self.name_entry = Entry(self.global_frame)
+        self.name_entry.grid(row=1, column=1, sticky=N+E+S+W)
+        #self.name_entry.pack(side=RIGHT)
+        self.name_entry.configure(fg="black")
+        #width=40)
         #self.name_entry.bind("<KeyRelease>", self.__name_keyboard_entry)
     
     # create Santa Frame
@@ -65,18 +68,21 @@ class Gui(Tk):
         self.santa_image_label = Label(self.global_frame)
         self.santa_image_label.grid(row=2, column=0, stick=W)
         #self.santa_image_label.pack(side=LEFT)
-        self.santa_image_label.configure(image=self.santa_image) 
+        self.santa_image_label.configure(image=self.santa_image, bg="#f33") 
         
     def __add_message_box_label(self):
-        self.message_box_label = Message(self.global_frame)
-        self.message_box_label.grid(row=2, column=0, columnspan=2, ipadx=80, ipady=30, padx=5)
-        self.message_box_label.configure(bg="#fff", fg="blue", font="Arial 8", relief=SUNKEN, padx=5)
+        #self.message_box_label = Message(self.global_frame)
+        self.message_box_label = Text(self.global_frame)
+        self.message_box_label.grid(row=2, column=1, sticky=E)
+        self.message_box_label.configure(bg="#fff", fg="blue", font="Arial 8", relief=SUNKEN, height=5, width=30)
+        #ipadx=80, ipady=30
 
     def __add_post_button(self):
         self.post_button = Button(self.global_frame)
-        self.post_button.grid(row=3, column=0, columnspan=2) 
+        self.post_button.grid(row=3, column=0, columnspan=2, sticky=N+E+S+W) 
         #sticky=N+E+S+W)
-        self.post_button.configure(bg="#ff0", text="Post Letter", width=50, padx=5)
+        self.post_button.configure(bg="#ff0", text="Post Letter")
+        #width=50, padx=5)
         self.post_button.bind("<Button-1>", self.__post_button_clicked)
 
     def __post_button_clicked(self, event):
